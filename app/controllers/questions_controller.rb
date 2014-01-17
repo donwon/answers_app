dcae174b841340439dc5e858c725c7ae80dc7f33
donwon <imdonw@gmail.com>
2014-01-17 14:48:46 -0800

    Integrate Bootstrap Create and Show method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,28 @@
 class QuestionsController < ApplicationController
 
 def index
-	@Question = Question.new
+#by default will just render the index.html.erb file
+	@questions = Question.all
 end
+
+def new
+	@question = Question.new
+end
+
+def create
+	Rails.logger.info params[:question]
+	question = Question.new params.require(:question).permit([:title, :body])
+	question.save
+
+	redirect_to questions_path
+
+end
+
+def show
+	@question = Question.find(params[:id])
+
+end
+
 
 
 end
